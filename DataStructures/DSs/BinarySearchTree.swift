635fd9475b798517f49f876122eaec5a47681ca6
Manas Mishra <manas1.mishra@ril.com>
2019-10-23 10:38:16 +0530

    BST started

---
+++ DataStructures/DSs/BinarySearchTree.swift
@@ -0,0 +1,16 @@
+//
+//  BinarySearchTree.swift
+//  DataStructures
+//
+//  Created by Manas Mishra on 23/10/19.
+//  Copyright © 2019 manas. All rights reserved.
+//
+
+import Foundation
+
+class BSTNode {
+    var data: Int?
+    var left: BSTNode?
+    var right: BSTNode?
+}
+
